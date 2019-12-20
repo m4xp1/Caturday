efba3e2a83fe7e50c0ac690ac6d7c8f5ba1f152f
m4xp1 <dm@xcorp.one>
2019-12-20 11:42:14 +0300

    Move catsApiKey from constructor to provide method arguments

--- data/src/main/java/one/xcorp/caturday/data/di/module/ApiModule.kt
+++ data/src/main/java/one/xcorp/caturday/data/di/module/ApiModule.kt
@@ -4,24 +4,24 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import one.xcorp.caturday.data.di.qualifier.CatsApiKey
 import one.xcorp.caturday.data.source.network.CatsApi
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
-class ApiModule(
-    private val catsApiKey: String
-) {
+internal class ApiModule {
 
     @Provides
     @Singleton
     fun provideCatsApi(
+        @CatsApiKey apiKey: String,
         httpClientBuilder: OkHttpClient.Builder,
         retrofitBuilder: Retrofit.Builder
     ): CatsApi {
         httpClientBuilder.addInterceptor {
             val request: Request = it.request().newBuilder()
-                .header(CatsApi.X_API_KEY_HEADER, catsApiKey)
+                .header(CatsApi.X_API_KEY_HEADER, apiKey)
                 .build()
             it.proceed(request)
         }
---
+++ data/src/main/java/one/xcorp/caturday/data/di/qualifier/CatsApiKey.kt
@@ -0,0 +1,7 @@
+package one.xcorp.caturday.data.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CatsApiKey
