b0805e115abfd60f461201d152e1adbb51f8e83e
m4xp1 <dm@xcorp.one>
2019-12-20 16:06:49 +0300

    Create component in application

--- app/src/main/java/one/xcorp/caturday/Application.kt
+++ app/src/main/java/one/xcorp/caturday/Application.kt
@@ -1,12 +1,15 @@
 package one.xcorp.caturday
 
 import android.app.Application
+import one.xcorp.caturday.data.di.DaggerDataComponent
+import one.xcorp.caturday.di.DaggerApplicationComponent
 
 class Application : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-
-
+    val component by lazy {
+        DaggerApplicationComponent.factory().create(
+            this,
+            DaggerDataComponent.factory().create(BuildConfig.CATS_API_KEY)
+        )
     }
 }
