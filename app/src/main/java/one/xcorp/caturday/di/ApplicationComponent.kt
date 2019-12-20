a77a3ea58d2eff385e26d74f9c8cf1f53ed201bc
m4xp1 <dm@xcorp.one>
2019-12-20 15:45:12 +0300

    Added ApplicationComponent

---
+++ app/src/main/java/one/xcorp/caturday/di/ApplicationComponent.kt
@@ -0,0 +1,29 @@
+package one.xcorp.caturday.di
+
+import android.app.Application
+import dagger.BindsInstance
+import dagger.Component
+import one.xcorp.caturday.data.di.DataComponent
+import one.xcorp.caturday.di.module.ApplicationModule
+import ru.cardsmobile.mw3.barch.presentation.di.scope.ApplicationScope
+
+@ApplicationScope
+@Component(
+    dependencies = [
+        DataComponent::class
+    ],
+    modules = [
+        ApplicationModule::class
+    ]
+)
+interface ApplicationComponent {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(
+            @BindsInstance application: Application,
+            dataComponent: DataComponent
+        ): ApplicationComponent
+    }
+}
