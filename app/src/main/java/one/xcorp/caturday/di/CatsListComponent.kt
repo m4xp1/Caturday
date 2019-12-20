5b49a65ad11e0c6d63bb2432319c8eab64eb8cee
m4xp1 <dm@xcorp.one>
2019-12-20 23:02:59 +0300

    Added CatsListModule

--- app/src/main/java/one/xcorp/caturday/di/CatsListComponent.kt
+++ app/src/main/java/one/xcorp/caturday/di/CatsListComponent.kt
@@ -1,11 +1,16 @@
 package one.xcorp.caturday.di
 
 import dagger.Subcomponent
+import one.xcorp.caturday.di.module.CatsListModule
 import one.xcorp.caturday.screen.cats.list.CatsListActivity
 import ru.cardsmobile.mw3.barch.presentation.di.scope.ActivityScope
 
 @ActivityScope
-@Subcomponent
+@Subcomponent(
+    modules = [
+        CatsListModule::class
+    ]
+)
 interface CatsListComponent {
 
     fun inject(activity: CatsListActivity)
---
+++ app/src/main/java/one/xcorp/caturday/di/module/CatsListModule.kt
@@ -0,0 +1,15 @@
+package one.xcorp.caturday.di.module
+
+import dagger.Binds
+import dagger.Module
+import one.xcorp.caturday.screen.cats.list.CatsListContract
+import one.xcorp.caturday.screen.cats.list.CatsListPresenter
+import ru.cardsmobile.mw3.barch.presentation.di.scope.ActivityScope
+
+@Module
+abstract class CatsListModule {
+
+    @Binds
+    @ActivityScope
+    abstract fun bindPresenter(presenter: CatsListPresenter): CatsListContract.Presenter
+}
