6e5545cc5c394d39fcf1bde074fde4d6b04d8d8f
m4xp1 <dm@xcorp.one>
2019-12-21 13:58:34 +0300

    Added injection presenter in CatsListActivity

--- app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListActivity.kt
+++ app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListActivity.kt
@@ -1,13 +1,41 @@
 package one.xcorp.caturday.screen.cats.list
 
 import android.os.Bundle
-import one.xcorp.caturday.activity.BaseActivity
+import one.xcorp.caturday.Application.Dependencies.graph
+import one.xcorp.caturday.Application.Dependencies.obtain
+import one.xcorp.caturday.Application.Dependencies.release
 import one.xcorp.caturday.R
+import one.xcorp.caturday.activity.BaseActivity
+import one.xcorp.caturday.model.CatModel
+import javax.inject.Inject
 
-class CatsListActivity : BaseActivity() {
+class CatsListActivity : BaseActivity(), CatsListContract.View {
+
+    @Inject
+    lateinit var presenter: CatsListContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        obtain(graph::catsList).inject(this)
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_cats_list)
+        presenter.attach(this)
+    }
+
+    override fun showError(throwable: Throwable) {
+
+    }
+
+    override fun showCats(items: List<CatModel>) {
+
+    }
+
+    override fun onDestroy() {
+        presenter.detach()
+
+        if (isFinishing) {
+            release(graph::catsList)
+        }
+        super.onDestroy()
     }
 }
