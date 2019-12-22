b7bda2a43cf6bbbf58a826782e65de8470e5df1e
m4xp1 <dm@xcorp.one>
2019-12-22 10:57:42 +0300

    Fix CatsApi

--- data/src/main/java/one/xcorp/caturday/data/source/network/CatsApi.kt
+++ data/src/main/java/one/xcorp/caturday/data/source/network/CatsApi.kt
@@ -3,12 +3,17 @@ package one.xcorp.caturday.data.source.network
 import one.xcorp.caturday.data.source.network.dto.CatImageDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import rx.Single
 
 internal interface CatsApi {
 
     @GET(IMAGES_SEARCH_METHOD)
-    fun searchImages(limit: Int, page: Int, order: String): Single<Response<List<CatImageDto>>>
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("page") page: Int,
+        @Query("order") order: String
+    ): Single<Response<List<CatImageDto>>>
 
     companion object {
 
