7af100662742fe3b3a43e9a2e7bb2da7e3b490bd
m4xp1 <dm@xcorp.one>
2019-12-22 18:01:24 +0300

    Added CatViewHolder

---
+++ app/src/main/java/one/xcorp/caturday/adapter/cats/CatViewHolder.kt
@@ -0,0 +1,35 @@
+package one.xcorp.caturday.adapter.cats
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import kotlinx.android.synthetic.main.list_item_cat.view.*
+import one.xcorp.caturday.GlideApp
+import one.xcorp.caturday.R
+import one.xcorp.caturday.model.CatModel
+
+class CatViewHolder private constructor(
+    view: View
+) : ViewHolder(view) {
+
+    fun bindTo(catModel: CatModel?) {
+        if (catModel == null) {
+            itemView.imageView.setImageDrawable(null)
+        } else {
+            GlideApp.with(itemView.context)
+                .load(catModel.image)
+                .into(itemView.imageView)
+        }
+    }
+
+    companion object {
+
+        operator fun invoke(parent: ViewGroup): CatViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.list_item_cat, parent, false)
+
+            return CatViewHolder(view)
+        }
+    }
+}
