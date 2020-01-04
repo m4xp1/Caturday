141291952cb2c833ad4f0e77c376b8c8bb54e06a
m4xp1 <dm@xcorp.one>
2020-01-04 19:05:46 +0300

    Replace GetCatsImagesUseCase on GetCatsListUseCase

---
+++ domain/src/main/java/one/xcorp/caturday/domain/usecase/GetCatsListUseCase.kt
@@ -0,0 +1,39 @@
+package one.xcorp.caturday.domain.usecase
+
+import one.xcorp.caturday.domain.entity.CatEntity
+import one.xcorp.caturday.domain.entity.OrderEntity
+import one.xcorp.caturday.domain.entity.PageEntity
+import one.xcorp.caturday.domain.repository.CatsRepository
+import rx.Single
+import javax.inject.Inject
+import kotlin.math.max
+
+class GetCatsListUseCase @Inject constructor(
+    private val catsRepository: CatsRepository
+) {
+
+    operator fun invoke(
+        size: Int,
+        position: Int,
+        order: OrderEntity,
+        sticky: Boolean
+    ): Single<PageEntity<CatEntity>> {
+        var request = catsRepository.getCatsImages(size, position, order)
+
+        if (sticky && position != 0) {
+            request = request.flatMap {
+                if (it.items.size == size) {
+                    Single.just(it)
+                } else {
+                    val extremePosition = max(0, it.totalCount - size)
+                    catsRepository.getCatsImages(size, extremePosition, order)
+                }
+            }
+        }
+
+        return request.map { page ->
+            val items = page.items.map { CatEntity(it.id, it.image) }
+            PageEntity(page.startPosition, page.totalCount, items)
+        }
+    }
+}
