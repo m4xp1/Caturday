83e01ad5b65fd8d166fc283b98e17a8052f6c567
m4xp1 <dm@xcorp.one>
2020-01-06 07:57:53 +0300

    Small refactor ComponentHolder

--- app/src/main/java/one/xcorp/caturday/dagger/holder/ComponentHolder.kt
+++ app/src/main/java/one/xcorp/caturday/dagger/holder/ComponentHolder.kt
@@ -2,7 +2,7 @@ package one.xcorp.caturday.dagger.holder
 
 interface ComponentHolder<Factory : Any, Component : Any> {
 
-    fun get(initializer: (Factory) -> Component): Component
+    fun get(initializer: Factory.() -> Component): Component
 
     fun get(): Component
 
