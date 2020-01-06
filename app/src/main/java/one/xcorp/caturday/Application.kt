9e15aaa40af950ded28ad8fc686aa3d19d821dab
m4xp1 <dm@xcorp.one>
2020-01-06 08:26:41 +0300

    Fix imports in Application

--- app/src/main/java/one/xcorp/caturday/Application.kt
+++ app/src/main/java/one/xcorp/caturday/Application.kt
@@ -1,7 +1,7 @@
 package one.xcorp.caturday
 
 import one.xcorp.caturday.dagger.ApplicationComponent
-import one.xcorp.caturday.dagger.component.DaggerApplicationComponent
+import one.xcorp.caturday.dagger.DaggerApplicationComponent
 import one.xcorp.caturday.data.dagger.DaggerDataComponent
 
 class Application : android.app.Application() {
