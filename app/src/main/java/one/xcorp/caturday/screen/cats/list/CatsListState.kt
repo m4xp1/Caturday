d991651d3d128895485d9e97dcf693164cd3a692
m4xp1 <dm@xcorp.one>
2020-01-06 08:46:26 +0300

    Parcelize CatsListState

--- app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListState.kt
+++ app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListState.kt
@@ -1,5 +1,8 @@
 package one.xcorp.caturday.screen.cats.list
 
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class CatsListState(
     override val initialKey: Int
 ) : CatsListContract.State
