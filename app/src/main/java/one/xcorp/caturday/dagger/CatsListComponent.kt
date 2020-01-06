7714099f9b1ab139c3eae2a56686409001d0a3ba
m4xp1 <dm@xcorp.one>
2020-01-06 07:03:50 +0300

    Added factory and holder in CatsListComponent

--- app/src/main/java/one/xcorp/caturday/dagger/CatsListComponent.kt
+++ app/src/main/java/one/xcorp/caturday/dagger/CatsListComponent.kt
@@ -1,8 +1,11 @@
 package one.xcorp.caturday.dagger
 
+import dagger.BindsInstance
 import dagger.Subcomponent
+import one.xcorp.caturday.dagger.holder.SingleComponentHolder
 import one.xcorp.caturday.dagger.module.CatsListModule
 import one.xcorp.caturday.screen.cats.list.CatsListActivity
+import one.xcorp.caturday.screen.cats.list.CatsListContract
 import ru.cardsmobile.mw3.barch.presentation.di.scope.ActivityScope
 
 @ActivityScope
@@ -14,4 +17,12 @@ import ru.cardsmobile.mw3.barch.presentation.di.scope.ActivityScope
 interface CatsListComponent {
 
     fun inject(activity: CatsListActivity)
+
+    @Subcomponent.Factory
+    interface Factory {
+
+        fun create(@BindsInstance state: CatsListContract.State?): CatsListComponent
+    }
+
+    class Holder(factory: Factory) : SingleComponentHolder<Factory, CatsListComponent>(factory)
 }
