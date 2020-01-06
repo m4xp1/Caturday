2b08921ff5597b5ffd30cfd4af049a3f41bde962
m4xp1 <dm@xcorp.one>
2020-01-06 06:59:46 +0300

    Added ComponentHolder

---
+++ app/src/main/java/one/xcorp/caturday/dagger/holder/ComponentHolder.kt
@@ -0,0 +1,10 @@
+package one.xcorp.caturday.dagger.holder
+
+interface ComponentHolder<Factory : Any, Component : Any> {
+
+    fun get(initializer: (Factory) -> Component): Component
+
+    fun get(): Component
+
+    fun release()
+}
---
+++ app/src/main/java/one/xcorp/caturday/dagger/holder/SingleComponentHolder.kt
@@ -0,0 +1,20 @@
+package one.xcorp.caturday.dagger.holder
+
+abstract class SingleComponentHolder<Factory : Any, Component : Any>(
+    private val factory: Factory
+) : ComponentHolder<Factory, Component> {
+
+    private var instance: Component? = null
+
+    override fun get(initializer: (Factory) -> Component): Component = synchronized(this) {
+        instance ?: initializer(factory).also { instance = it }
+    }
+
+    override fun get(): Component = synchronized(this) {
+        requireNotNull(instance) { "Requested component not initialized." }
+    }
+
+    override fun release() = synchronized(this) {
+        instance = null
+    }
+}
