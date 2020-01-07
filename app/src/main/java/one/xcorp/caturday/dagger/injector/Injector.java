0c99d14b36418e8af71b9cc95ca3fa21a228ec46
m4xp1 <dm@xcorp.one>
2020-01-07 03:26:11 +0300

    Added NonNull annotation in Injector

--- app/src/main/java/one/xcorp/caturday/dagger/injector/Injector.java
+++ app/src/main/java/one/xcorp/caturday/dagger/injector/Injector.java
@@ -1,7 +1,9 @@
 package one.xcorp.caturday.dagger.injector;
 
+import androidx.annotation.NonNull;
+
 // TODO: replace on Kotlin class when obtain SAM constructor support in Kotlin 1.4
 public interface Injector<Component> {
 
-    void invoke(Component component);
+    void invoke(@NonNull Component component);
 }
