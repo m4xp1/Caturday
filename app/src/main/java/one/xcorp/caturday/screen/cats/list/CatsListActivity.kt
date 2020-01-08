06163cdbe8b45f3922af6981269297fe93f45476
m4xp1 <dm@xcorp.one>
2020-01-08 07:25:50 +0300

    Added load cats list with pagination

--- app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListActivity.kt
+++ app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListActivity.kt
@@ -1,12 +1,24 @@
 package one.xcorp.caturday.screen.cats.list
 
 import android.os.Bundle
-import one.xcorp.caturday.Application.Dependencies.graph
-import one.xcorp.caturday.Application.Dependencies.obtain
-import one.xcorp.caturday.Application.Dependencies.release
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_cats_list.*
+import one.xcorp.caturday.Application.Dependencies.applicationComponent
+import one.xcorp.caturday.BaseActivity
 import one.xcorp.caturday.R
-import one.xcorp.caturday.activity.BaseActivity
-import one.xcorp.caturday.model.CatModel
+import one.xcorp.caturday.dagger.CatsListComponent
+import one.xcorp.caturday.dagger.holder.injectWith
+import one.xcorp.caturday.dagger.injector.Injector
+import one.xcorp.caturday.screen.cats.list.adapter.CatsListAdapter
+import one.xcorp.caturday.screen.cats.list.model.CatModel
+import one.xcorp.caturday.screen.cats.list.model.StateModel
+import one.xcorp.caturday.screen.cats.list.model.StateModel.*
 import javax.inject.Inject
 
 class CatsListActivity : BaseActivity(), CatsListContract.View {
@@ -14,28 +26,75 @@ class CatsListActivity : BaseActivity(), CatsListContract.View {
     @Inject
     lateinit var presenter: CatsListContract.Presenter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        obtain(graph::catsList).inject(this)
-        super.onCreate(savedInstanceState)
+    private val adapter by lazy { CatsListAdapter() }
 
+    private val injector = Injector<CatsListComponent> { it.inject(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cats_list)
+
+        configureRecyclerView()
+
         presenter.attach(this)
     }
 
-    override fun showError(throwable: Throwable) {
+    override fun onInject(savedInstanceState: Bundle?) = applicationComponent
+        .catsListComponentHolder
+        .injectWith(injector) {
+            createComponent(
+                savedInstanceState?.getParcelable(KEY_CATS_LIST_CONTRACT_PRESENTER_STATE)
+            )
+        }
 
+    private fun configureRecyclerView() {
+        val spanCount = resources.getInteger(R.integer.span_count)
+
+        recyclerView.layoutManager = GridLayoutManager(this, spanCount)
+        recyclerView.adapter = adapter
     }
 
-    override fun showCats(items: List<CatModel>) {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_CATS_LIST_CONTRACT_PRESENTER_STATE, presenter.getState())
+    }
 
+    override fun showCatsListState(state: StateModel) = when (state) {
+        is Running -> {
+            progressView.visibility = if (state.isInitial) VISIBLE else GONE
+            recyclerView.visibility = if (state.isInitial) GONE else VISIBLE
+        }
+        is Failed -> {
+            Snackbar.make(recyclerView, state.message, LENGTH_INDEFINITE)
+                .setAction(R.string.retry_button) { state.retry() }
+                .show()
+        }
+        is Success -> {
+            progressView.visibility = GONE
+            recyclerView.visibility = VISIBLE
+        }
+    }
+
+    override fun showCatsList(list: PagedList<CatModel>) {
+        adapter.submitList(list)
     }
 
     override fun onDestroy() {
         presenter.detach()
 
         if (isFinishing) {
-            release(graph::catsList)
+            presenter.dispose()
         }
         super.onDestroy()
+    }
+
+    private companion object {
+
+        private const val KEY_CATS_LIST_CONTRACT_PRESENTER_STATE =
+            "CatsListContract.Presenter.State"
+    }
+
+    fun testClick(view: View) {
+        presenter.invalidate()
     }
 }
--- app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListPresenter.kt
+++ app/src/main/java/one/xcorp/caturday/screen/cats/list/CatsListPresenter.kt
@@ -1,14 +1,97 @@
 package one.xcorp.caturday.screen.cats.list
 
-import one.xcorp.caturday.domain.usecase.GetCatsImagesUseCase
-import one.xcorp.caturday.presenter.BasePresenterImpl
+import android.content.Context
+import androidx.core.content.ContextCompat.getMainExecutor
+import androidx.paging.PagedList
+import one.xcorp.caturday.BasePresenterImpl
+import one.xcorp.caturday.data.paging.RxPositionalDataSource
+import one.xcorp.caturday.screen.cats.list.mapper.toStateModel
+import one.xcorp.caturday.screen.cats.list.model.CatModel
+import one.xcorp.caturday.screen.cats.list.model.StateModel
+import one.xcorp.caturday.screen.cats.list.model.StateModel.Running
+import rx.android.schedulers.AndroidSchedulers
+import rx.lang.kotlin.plusAssign
+import rx.schedulers.Schedulers
+import rx.subjects.PublishSubject
+import java.util.concurrent.Executors.newSingleThreadExecutor
 import javax.inject.Inject
+import javax.inject.Provider
 
 class CatsListPresenter @Inject constructor(
-    private val getCatsImages: GetCatsImagesUseCase
-) : BasePresenterImpl<CatsListContract.View>(), CatsListContract.Presenter {
+    private val applicationContext: Context,
+    private val savedInstanceState: CatsListContract.State?,
+    private val catsListDataSourceProvider: Provider<RxPositionalDataSource<CatModel>>
+) : BasePresenterImpl<CatsListContract.View, CatsListContract.State>(), CatsListContract.Presenter {
 
-    override fun nextPage() {
+    private val catsListStateSubject = PublishSubject.create<StateModel>()
+    private val catsListStateObservable = catsListStateSubject
+        .startWith(Running(true))
+        .distinctUntilChanged()
+        .replay(1)
 
+    private val createCatsListSubject = PublishSubject.create<Int>()
+    private val createCatsListObservable = createCatsListSubject
+        .startWith(getInitialPosition())
+        .observeOn(Schedulers.io())
+        .map<PagedList<CatModel>> { createCatsList(it) }
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnNext { catsList = it }
+        .replay(1)
+
+    private var catsList: PagedList<CatModel>? = null
+
+    init {
+        presenterSubscriptions += catsListStateObservable.connect()
+        presenterSubscriptions += createCatsListObservable.connect()
+    }
+
+    override fun attach(view: CatsListContract.View) {
+        super.attach(view)
+
+        viewSubscriptions += catsListStateObservable.subscribe {
+            view.showCatsListState(it)
+        }
+
+        viewSubscriptions += createCatsListObservable.subscribe {
+            view.showCatsList(it)
+        }
+    }
+
+    override fun getState(): CatsListContract.State {
+        return CatsListState(getInitialPosition())
+    }
+
+    private fun createCatsList(initialKey: Int = 0, pageSize: Int = 10): PagedList<CatModel> {
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(true)
+            .setPageSize(pageSize)
+            .build()
+
+        return PagedList.Builder<Int, CatModel>(createCatsListDataSource(), config)
+            .setInitialKey(initialKey)
+            .setFetchExecutor(newSingleThreadExecutor())
+            .setNotifyExecutor(getMainExecutor(applicationContext))
+            .build()
+    }
+
+    private fun createCatsListDataSource(): RxPositionalDataSource<CatModel> {
+        val dataSource = catsListDataSourceProvider.get()
+
+        dataSource.observeLoading()
+            .map { it.toStateModel() }
+            .subscribe { catsListStateSubject.onNext(it) }
+
+        dataSource.addInvalidatedCallback {
+            createCatsListSubject.onNext(getInitialPosition())
+        }
+
+        return dataSource
+    }
+
+    private fun getInitialPosition(): Int =
+        catsList?.lastKey as? Int ?: savedInstanceState?.initialPosition ?: 0
+
+    override fun invalidate() {
+        catsList?.dataSource?.invalidate()
     }
 }
