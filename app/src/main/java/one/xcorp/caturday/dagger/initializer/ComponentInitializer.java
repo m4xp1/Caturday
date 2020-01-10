f979df2ac73d6ddec70f67c2ad46f8a6c62c5dfe
m4xp1 <dm@xcorp.one>
2020-01-10 04:41:56 +0300

    Added TODO on ComponentInitializer

--- app/src/main/java/one/xcorp/caturday/dagger/initializer/ComponentInitializer.java
+++ app/src/main/java/one/xcorp/caturday/dagger/initializer/ComponentInitializer.java
@@ -2,6 +2,7 @@ package one.xcorp.caturday.dagger.initializer;
 
 import androidx.annotation.NonNull;
 
+// TODO: replace on Kotlin class when obtain SAM constructor support in Kotlin 1.4
 public interface ComponentInitializer<Factory, Component> {
 
     @NonNull
